d2907ad0f205f5363e5a6a63e1e067daa80bd145
botOliver <botoliver556@gmail.com>
2026-02-26 16:04:36 +0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 
 function App() {
+	const [tasks, setTasks] = useState([])
 	const [openSection, setOpenSection] = useState({
-		taskList: false,
+		taskList: true,
 		tasks: true,
 		completedForm: true,
 	})
 	function toggleSection(section) {
 		setOpenSection(prev => ({ ...prev, [section]: !prev[section] }))
 	}
+
+	function addTask(task) {
+		setTasks([...tasks, { ...task, completed: false, id: Date.now() }])
+	}
+
+	const activeTasks = tasks.filter(task => !task.completed)
+	const completedTasks = tasks.filter(task => task.completed)
 
 	return (
 		<div className='app'>
@@ -20,7 +28,7 @@ function App() {
 				>
 					+
 				</button>
-				{openSection.taskList && <TaskForm />}
+				{openSection.taskList && <TaskForm addTask={addTask} />}
 			</div>
 
 			<div className='task-container'>
@@ -35,7 +43,7 @@ function App() {
 					<button className='sort-button'>By Date</button>
 					<button className='sort-button'>By Priority</button>
 				</div>
-				{openSection.tasks && <TaskList />}
+				{openSection.tasks && <TaskList activeTasks={activeTasks} />}
 			</div>
 
 			<div className='completed-task-container'>
@@ -54,48 +62,77 @@ function App() {
 }
 
 /// Тут компонент под форму
-function TaskForm() {
+function TaskForm({ addTask }) {
+	const [title, setTitle] = useState('')
+	const [priority, setPriority] = useState('Low')
+	const [deadline, setDeadline] = useState('')
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		if (title.trim() && deadline) {
+			;(addTask({ title, priority, deadline }),
+				setTitle(''),
+				setPriority('Low'),
+				setDeadline(''))
+		}
+	}
+
 	return (
-		<form action='' className='task-form'>
-			<input type='text' value={''} placeholder='Task Title' required />
-			<select value={''}>
+		<form action='' className='task-form' onSubmit={handleSubmit}>
+			<input
+				type='text'
+				value={title}
+				placeholder='Task Title'
+				required
+				onChange={e => setTitle(e.target.value)}
+			/>
+			<select value={priority} onChange={e => setPriority(e.target.value)}>
 				<option value='High'>High</option>
-				<option value='Mediun'>Mediun</option>
+				<option value='Medium'>Medium</option>
 				<option value='Low'>Low</option>
 			</select>
-			<input type='datetime-local' required value={''} />
+			<input
+				type='datetime-local'
+				required
+				value={deadline}
+				onChange={e => setDeadline(e.target.value)}
+			/>
 			<button type='submit'>Add Task</button>
 		</form>
 	)
 }
 
 /// Тут компонент подвторую форму
-function TaskList() {
+function TaskList({ activeTasks }) {
+	//в этот массив мы передалаи activeTasks в котором незавершенный таски
+	console.log(activeTasks)
 	return (
 		<ul className='task-list'>
-			<TaskItem />
+			{activeTasks.map(task => (
+				<TaskItem task={task} key={task.id} />
+			))}
 		</ul>
 	)
 }
 
 /// Тут компонент выполненные задачи
-function CompletedTaskList() {
-	return (
-		<ul className='completed-task-list'>
-			<TaskItem />
-		</ul>
-	)
+function CompletedTaskList({ completedTasks }) {
+	return <ul className='completed-task-list'>{/* <TaskItem /> */}</ul>
 }
 
 /// Тут мы делаем компоненты (элементы), которые будут в списке, который выше.
-function TaskItem() {
+function TaskItem({ task }) {
+	console.log()
+	const { title, priority, deadline, id } = task
 	return (
-		<li className='task-item'>
+		<li className={`task-item ${priority.toLowerCase()}`}>
 			<div className='task-info'>
 				<div>
-					Title <strong>Medium</strong>
+					{title} <strong>{priority}</strong>
 				</div>
-				<div className='task-deadline'>Due: {new Date().toLocaleString()}</div>
+				<div className='task-deadline'>
+					Due: {new Date(deadline).toLocaleString()}
+				</div>
 			</div>
 			<div className='task-buttons'>
 				<button className='complete-button'>Complete</button>
